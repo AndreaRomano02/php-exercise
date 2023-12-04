7b9bfc2164d638094b55f42b7728bdc914e5a959
AndreaRomano02 <andy.romano2002@gmail.com>
2023-12-04 18:14:24 +0100

    Add Grafic and validation fix

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+$register = false;
+
 $email = $_POST['email'] ?? "";
 $password = $_POST['password'] ?? "";
 
@@ -18,20 +20,20 @@ $number    = preg_match('@[0-9]@', $password);  // If there are the number lette
 $specialChars = preg_match('@[^\w]@', $password);  // If there are a special charapter letter return 1 else 0
 
 //# Validate email
-if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+if (!strlen($email) && !strlen($password)  || filter_var($email, FILTER_VALIDATE_EMAIL)) {
     $class['email'] = "is-valid";
-} else {
+} else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     $message['email'] =  ("$email is not a valid email address");
     $class['email'] = "is-invalid";
 }
 
 
 //# Validate Password
-if (!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
+if (!strlen($email) && !strlen($password)) {
+    $class['password'] = 'is-valid';
+} else if (!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
     $message['password'] =  'Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character.';
     $class['password'] = 'is-invalid';
-} else {
-    $class['password'] = 'is-valid';
 }
 ?>
 
@@ -42,30 +44,58 @@ if (!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password
 <?php include './includes/head.html' ?>
 
 <body>
-    <div class="container my-5">
+    <section class="vh-100" style="background-color: #eee;">
+        <div class="container h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-lg-12 col-xl-11">
+                    <div class="card text-black" style="border-radius: 25px;">
+                        <div class="card-body p-md-5">
+                            <div class="row justify-content-center">
+                                <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
-        <form action="" method="POST">
+                                    <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
 
-            <!-- Email -->
-            <label for="email">Email</label>
-            <div class="mb-3">
-                <input type="email" id="email" name="email" class="d-block form-control <?= $class['email'] ?>">
-                <div>
-                    <small class="text-danger"><?= $message['email'] ?></small>
+                                    <form action="" method="POST">
+
+                                        <!-- Email -->
+                                        <div class="d-flex flex-row align-items-center mb-4">
+                                            <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                                            <div class="form-outline flex-fill mb-0">
+                                                <label class="form-label" for="email">Your Email</label>
+                                                <input value="<?= $email ?>" type="email" id="email" class="form-control <?= $class['email'] ?>" name="email" />
+                                                <div>
+                                                    <small class="text-danger"><?= $message['email'] ?></small>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <!-- Password -->
+                                        <div class="d-flex flex-row align-items-center mb-4">
+                                            <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                                            <div class="form-outline flex-fill mb-0">
+                                                <input type="password" id="password" class="form-control <?= $class['password'] ?>" name="password" />
+                                                <label class="form-label" for="password">Password</label>
+                                                <div>
+                                                    <small class="text-danger"><?= $message['password'] ?></small>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <!-- Button submit -->
+                                        <div class="d-flex justify-content-between ">
+
+                                            <button class="btn btn-primary btn-lg">Login</button>
+                                            <a href="./register.php">Registrati</a>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            <!-- Password -->
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" class="d-block form-control <?= $class['email'] ?>">
-            <div>
-                <small class="text-danger"><?= $message['password'] ?></small>
-            </div>
-
-            <!-- Button submit -->
-            <button class="btn btn-success">Login</button>
-        </form>
-    </div>
+        </div>
+    </section>
 </body>
 
 </html>
